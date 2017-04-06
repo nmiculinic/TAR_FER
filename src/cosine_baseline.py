2798099f4bcf5d4329e21ace18e4cfbc2fabe56b
Neven Miculinic <neven.miculinic@gmail.com>
2017-04-06 20:28:49 +0200

    Moved cosine_baseline to new API

--- src/cosine_baseline.py
+++ src/cosine_baseline.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cosine
+import pandas as pd
+import nltk
+import numpy as np
+import matplotlib.pyplot as plt
+from data import eval_ds, sentence_to_vector
+from model_utils import Model
 
-from data import *
+class SimpleBaseline(Model):
+    porter = nltk.PorterStemmer()
 
-data = EvaluationData()
+    def _predict(self, a, b):
+        fn = np.sum
+        sem1 = fn(a, axis=0)
+        sem2 = fn(b, axis=0)
+        return cosine(sem1, sem2)
 
-cos_similarities = []
-for i, (sent1, sent2) in enumerate(data.input_vectors):
-    # we need a way to aggregate word embeddings within a sentence. np.mean? np.sum? a deep neural network?
-    fn = np.sum
-    sem1 = fn(sent1, axis=0)
-    sem2 = fn(sent2, axis=0)
-    cos_similarities.append(cosine(sem1, sem2))
-cos_similarities = np.array(cos_similarities)
+    def preprocessLine(self, sentence):
+        return sentence_to_vector(nltk.word_tokenize(sentence))
 
-pearson_corr = np.corrcoef([data.target, cos_similarities])
-print("Correlation coefficitent:", pearson_corr[0, 1])
-plt.plot(data.target, cos_similarities, "o")
-plt.show()
+model = SimpleBaseline("log_cosine", eval_ds, {'log_overwrite':True})
+model.evalScore()
